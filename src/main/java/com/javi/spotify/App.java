4cae50894e5940f8041c4f45355b13137128797b
Javier González <javier_3791@hotmail.com>
2018-08-01 20:01:14 -0400

    Initial commit

---
+++ src/main/java/com/javi/spotify/App.java
@@ -0,0 +1,13 @@
+package com.javi.spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
